114a3c234f7ed09a6c67750ac8ed422f8c35f176
Jake Eckhardt <jake.c.eckhardt@gmail.com>
2016-12-27 21:39:23 -0800

    NOW that last crap is commited.

---
+++ js/styles.js
@@ -0,0 +1,11 @@
+$(window).load(function() {
+  $("body").removeClass("preload");
+});
+
+
+
+
+$(".menu_button").click(function(event) {
+  $('#menu_button_container').toggleClass("pushed");
+  $("#nav").toggleClass("nav-left");
+});
