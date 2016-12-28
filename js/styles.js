33e0bd5b7af31561e10f59d3e3f59272534378c2
Jake Eckhardt <jake.c.eckhardt@gmail.com>
2016-12-28 00:27:53 -0800

    Transitioned title with menu. Added background.

--- js/styles.js
+++ js/styles.js
@@ -8,4 +8,5 @@ $(window).load(function() {
 $(".menu_button").click(function(event) {
   $('#menu_button_container').toggleClass("pushed");
   $("#nav").toggleClass("nav-left");
+  $("#logo").toggleClass("logo-left");
 });
