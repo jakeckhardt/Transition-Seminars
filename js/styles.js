50e62ef0eb054f145ac4e8333d5d0a01d19ac6bb
Jake Eckhardt <jake.c.eckhardt@gmail.com>
2017-01-02 15:26:05 -0800

    Finished vision and strategy statement complete with fade ins

--- js/styles.js
+++ js/styles.js
@@ -3,19 +3,62 @@ $(window).load(function() {
 });
 
 
-
+/*Menu slide left on burger click*/
 
 $(".menu_button").click(function(event) {
   $('#menu_button_container').toggleClass("pushed");
   $("#nav").toggleClass("nav-left");
-  $("#logo").toggleClass("logo-left");
 });
 
 
-/*
+/*Burger color change on scroll*/
+
+var intro = $("#intro").offset();
+
 $(window).scroll(function() {
-	$(window).scroll( 1080, function() {
-		$("#quote").fadeOut();
-	})
+    if ($(window).scrollTop() >= intro.top) {
+        $(".bar").addClass("bar-scroll");
+    } else {
+        $(".bar").removeClass("bar-scroll");
+    }
 });
-*/
+
+
+/* Fade in effects*/
+
+$(window).scroll(function() {
+  if ($(window).scrollTop()>600) {
+		$("#vision img").addClass("animated fadeInUp");
+    $("#vision span").addClass("animated fadeInUp");
+    $("#vision p").addClass("animated fadeInUp");
+	} else {
+
+  }
+
+  if ($(window).scrollTop()>1000) {
+    $("#value-photos img").addClass("animated fadeInUp");
+  } else {
+
+  }
+
+  if ($(window).scrollTop()>1500) {
+    $("#strategy img").addClass("animated fadeInUp");
+    $("#strategy span").addClass("animated fadeInUp");
+    $("#strategy p").addClass("animated fadeInUp");
+  } else {
+
+  }
+});
+
+
+
+
+		
+	
+
+
+
+
+
+
+
